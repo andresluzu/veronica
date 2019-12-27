7a30d4a3a218156a4fd569e360a903447ef27cae
Jorge Ayala <jorge.ayala@safike.com>
2019-12-27 11:41:47 -0500

    SEL-827

--- veronica-app/src/main/java/com/rolandopalermo/facturacion/ec/app/VeronicaApplication.java
+++ veronica-app/src/main/java/com/rolandopalermo/facturacion/ec/app/VeronicaApplication.java
@@ -3,13 +3,12 @@ package com.rolandopalermo.facturacion.ec.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @ComponentScan({"com.rolandopalermo.facturacion.ec"})
 @EnableJpaRepositories(basePackages = {"com.rolandopalermo.facturacion.ec.persistence.repository"})
 @EntityScan(basePackages = {"com.rolandopalermo.facturacion.ec.persistence.entity"})
